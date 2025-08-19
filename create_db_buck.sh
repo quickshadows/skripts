9eb5bf7c931e93a1607af1372ead9832cb97602c
Mihail <mihailutchev@gmail.com>
2025-08-19 16:39:42 +0300

    ee

--- create_db_buck.sh
+++ create_db_buck.sh
@@ -70,43 +70,18 @@ create_database() {
 floating_ip=$(get_floating_ip)
 
 # Создание баз данных для разных версий
-create_database "MySQL80 api stage" "mysql" "519" "192.168.0.201"
+create_database "MySQL80 api buckup" "mysql" "519" "192.168.0.211"
 floating_ip=$(get_floating_ip)
-create_database "MySQL84 api stage" "mysql8_4" "519" "192.168.0.202"
-floating_ip=$(get_floating_ip)
-
-create_database "PostgreSQL 14 api stage" "postgres14" "1175" "192.168.0.203"
-floating_ip=$(get_floating_ip)
-create_database "PostgreSQL 15 api stage" "postgres15" "1175" "192.168.0.203"
-floating_ip=$(get_floating_ip)
-create_database "PostgreSQL 16 api stage" "postgres16" "1175" "192.168.0.204"
-floating_ip=$(get_floating_ip)
-create_database "PostgreSQL 17 api stage" "postgres17" "1175" "192.168.0.205"
+create_database "MySQL84 api buckup" "mysql8_4" "519" "192.168.0.212"
 floating_ip=$(get_floating_ip)
 
-create_database "redis7 api stage" "redis7" "541" "192.168.0.206"
+create_database "PostgreSQL 14 api buckup" "postgres14" "1175" "192.168.0.213"
 floating_ip=$(get_floating_ip)
-create_database "redis81 api stage" "redis8_1" "541" "192.168.0.207"
+create_database "PostgreSQL 15 api buckup" "postgres15" "1175" "192.168.0.213"
 floating_ip=$(get_floating_ip)
-
-create_database "mongodb7 api stage" "mongodb7" "515" "192.168.0.208"
+create_database "PostgreSQL 16 api buckup" "postgres16" "1175" "192.168.0.214"
 floating_ip=$(get_floating_ip)
-create_database "mongodb8 api stage" "mongodb8_0" "515" "192.168.0.209"
+create_database "PostgreSQL 17 api buckup" "postgres17" "1175" "192.168.0.215"
 floating_ip=$(get_floating_ip)
-
-create_database "opensearch2_19 api stage" "opensearch2_19" "747" "192.168.0.210"
-floating_ip=$(get_floating_ip)
-
-create_database "clickhouse api stage" "clickhouse" "1229" "192.168.0.211"
-floating_ip=$(get_floating_ip)
-create_database "clickhouse api stage" "clickhouse24" "1229" "192.168.0.212"
-floating_ip=$(get_floating_ip)
-create_database "clickhouse api stage" "clickhouse25" "1229" "192.168.0.213"
-floating_ip=$(get_floating_ip)
-
-create_database "kafka api stage" "kafka" "759" "192.168.0.214"
-floating_ip=$(get_floating_ip)
-
-create_database "rabbitmq4_0 api stage" "rabbitmq4_0" "805" "192.168.0.215"
 
 
