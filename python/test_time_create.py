11a02b69ecef54709f126fe9ca1f389584bb61cd
qa-testq <m.uchaev@timeweb.ru>
2025-09-02 11:44:52 +0300

    Update test_time_create.py

--- python/test_time_create.py
+++ python/test_time_create.py
@@ -2,40 +2,42 @@ import psycopg2
 import time
 from datetime import datetime
 
-#Настройки подключения
+# Настройки подключения
 DB_NAME = ""
 USER = ""
 PASSWORD = ""
 HOST = ""
+
+# Функция для создания таблицы
 def create_table(cursor):
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    tamle_name = f"users_{timestamp}"
+    table_name = f"users_{timestamp}"
 
     cursor.execute(f"""
-    CREATE TABLE {tamle_name} (
-    id DERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    CREATE TABLE {table_name} (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) NOT NULL,
+        email VARCHAR(100) NOT NULL UNIQUE,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
     """)
+    
+    print(f"Создана таблица: {table_name} в {timestamp}")
 
-    print(f"создана таблица: {table_name} в {tablestamp}")
+# Основной цикл
+try:
+    connection = psycopg2.connect(dbname=DB_NAME, user=USER, password=PASSWORD, host=HOST)
+    cursor = connection.cursor()
 
-    # Основной цикл
-    try:
-        connection = psycopg2.connect(dbname=DB_NAME, user=USER. password=PASSWORD, host=HOST)
-        cursor = connection.cursor()
-
-        while True
+    while True:
         create_table(cursor)
-        connection.commit()
-        time.sleep(5)
+        connection.commit()  # Подтверждение изменений
+        time.sleep(5)  # Задержка в 5 секунд между созданием таблиц
 
-    except KeyboardInterruptL:
-        print("Остановка скрипта.")
-    finally:
-        if cursor:
-            cursor
-        if connection
+except KeyboardInterrupt:
+    print("Остановка скрипта.")
+finally:
+    if cursor:
+        cursor.close()
+    if connection:
         connection.close()
