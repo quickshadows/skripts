d98146739543c73eab81614dd979b31d1eb3bde3
Mihail <mihailutchev@gmail.com>
2025-09-15 16:07:29 +0300

    Update delete_multipart.py

--- python/delete_multipart.py
+++ python/delete_multipart.py
@@ -5,9 +5,12 @@ aws_access_key_id = "XA2UUWUEW2WKT3IIDZ1Z"
 aws_secret_access_key = "InwYkp2JtmzW96NpMfdDZFqyQGSWfA8JsDDMG8N2"
 REGION = "ru-1"
 BUCKET_NAME = "31d5eb06-baket-backup-test"
+ENDPOINT_URL = "https://s3.twcstorage.ru"
+
 
 # Создаем сессию и клиента S3
 session = boto3.Session(
+    endpoint_url=ENDPOINT_URL,
     aws_access_key_id=aws_access_key_id,
     aws_secret_access_key=aws_secret_access_key,
     region_name=REGION
